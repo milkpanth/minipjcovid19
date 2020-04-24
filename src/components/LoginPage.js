21a3fbec0e4b81c3912fd01ece5e5abf31e6978d
milkpanth <milkpanth@yahoo.com>
2020-04-24 15:31:07 +0700

    lohin css

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,43 +1,116 @@
 import React, { useState } from 'react'
-import firebase from 'firebase/app';
+import firebase from 'firebase/app'
  import fire from '../config/fire'
+ import './loginPage.css'
+ import 'bootstrap/dist/css/bootstrap.min.css'
+ import { Nav, Navbar, Button, Form, FormControl } from 'react-bootstrap'
 
 function LoginPage () {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
   
-  const signin = (e) =>{
-     e.preventDefault();
-     fire.auth().signInWithEmailAndPassword(email, password).then((u) => {
-         console.log('usingin',u)
-     }).catch((err) => {
+    const signin = e => {
+        e.preventDefault()
+        fire
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+          .then(u => {
+            console.log('usingin', u)
+          })
+          .catch(err => {
          console.log(err)
      })
    }
    const signup = e => {
 
      e.preventDefault()
-     fire.auth().createUserWithEmailAndPassword(email, password).then((u) => {
-         console.log('usingup',u)
-     }).catch((err) => {
+     fire
+       .auth()
+       .createUserWithEmailAndPassword(email, password)
+       .then(u => {
+         console.log('usingup', u)
+       })
+       .catch(err => {
          console.log(err)
      })
  }
   return (
     <div>
-      LoginPage
-      <div>
-      <input type="text" name="email" onChange={e => setEmail(e.target.value)}></input>
+      <Navbar>
+         <Navbar.Brand href='/'>Navbar with text</Navbar.Brand>
+         <Navbar.Toggle />
+         <Navbar.Collapse className='justify-content-end'>
+           <Navbar.Text>
+             Signed in as: <a href='#login'>Mark Otto</a>
+           </Navbar.Text>
+         </Navbar.Collapse>
+       </Navbar>
+       <div className='sidenav'>
+         <div className='login-main-text'>
+           <h2>Application</h2>
+           <br></br>
+           <h2>Login Page</h2>
+           <p>Login or register from here to access.</p>
+         </div>
       </div>
-      <div>
-      <input type="password" name="password" onChange={e => setPassword(e.target.value)}
-        ></input>
-      </div>
-      <div>
-      <button type='submit' onClick={signin}>SingIn</button>
-         <button type='submit' onClick={signup}>Signup</button>
-      </div>
-    </div>
+      <div className='main'>
+         <div className='col-md-6 col-sm-12'>
+           <div className='login-form'>
+             <form>
+               <div className='form-group'>
+                 <label>User Name</label>
+                 <input
+                   type='text'
+                   onChange={e => setEmail(e.target.value)}
+                   className='form-control'
+                   placeholder='User Name'
+                 ></input>
+               </div>
+
+               <div className='form-group'>
+                 <label>Password</label>
+                 <input
+                   type='password'
+                   onChange={e => setPassword(e.target.value)}
+                   className='form-control'
+                   placeholder='Password'
+                 ></input>
+               </div>
+               <button type='submit' onClick={signin} className='btn btn-black'>
+                 Login
+               </button>
+               <button type='submit' onClick={signup} className='btn btn-secondary'>
+                 Register
+               </button>
+             </form>
+           </div>
+         </div>
+       </div>
+       {/* <div className='Login-header'>
+         <div>
+           <input
+             type='text'
+             name='email'
+             onChange={e => setEmail(e.target.value)}
+           ></input>
+         </div>
+         <div>
+           <input
+             type='password'
+             name='password'
+             onChange={e => setPassword(e.target.value)}
+           ></input>
+         </div>
+         <div>
+           <button type='submit' onClick={signin}>
+             SingIn
+           </button>
+           <button type='submit' onClick={signup}>
+             Signup
+           </button>
+         </div>
+       </div> */}
+     </div>
   )
 }
 export default LoginPage;
