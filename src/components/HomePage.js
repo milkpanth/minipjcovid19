e8beb9f1ba67a127cf43941b021b76de614d8b1c
milkpanth <milkpanth@yahoo.com>
2020-04-24 15:51:06 +0700

    topbar on hp

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import fire from '../config/fire';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Nav, Navbar, Button, Form, FormControl } from 'react-bootstrap'
 
- function HomePage () {
+function HomePage() {
     const logout = () => {
         fire.auth().signOut();
     }
-     return(
-         <div>
-             HomePage
-             <button onClick={logout}>Logout</button>
-         </div>
-     )
- }
- export default HomePage;
+    return (
+        <div>
+            <Navbar>
+                <Navbar.Brand href='/'>Navbar with text</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className='justify-content-end'>
+                    <Navbar.Text>
+                        Signed in as: <a href='#login'>Mark Otto</a>
+                    </Navbar.Text>
+                </Navbar.Collapse>
+            </Navbar>
+            <button onClick={logout}>Logout</button>
+        </div>
+    )
+}
+export default HomePage;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,92 +1,83 @@
 import React, { useState } from 'react'
 import firebase from 'firebase/app'
- import fire from '../config/fire'
- import './loginPage.css'
- import 'bootstrap/dist/css/bootstrap.min.css'
- import { Nav, Navbar, Button, Form, FormControl } from 'react-bootstrap'
+import fire from '../config/fire'
+import './loginPage.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Nav, Navbar, Button, Form, FormControl } from 'react-bootstrap'
 
-function LoginPage () {
+function LoginPage() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-  
+
     const signin = e => {
         e.preventDefault()
         fire
-          .auth()
-          .signInWithEmailAndPassword(email, password)
-          .then(u => {
-            console.log('usingin', u)
-          })
-          .catch(err => {
-         console.log(err)
-     })
-   }
-   const signup = e => {
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(u => {
+                console.log('usingin', u)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    const signup = e => {
 
-     e.preventDefault()
-     fire
-       .auth()
-       .createUserWithEmailAndPassword(email, password)
-       .then(u => {
-         console.log('usingup', u)
-       })
-       .catch(err => {
-         console.log(err)
-     })
- }
-  return (
-    <div>
-      <Navbar>
-         <Navbar.Brand href='/'>Navbar with text</Navbar.Brand>
-         <Navbar.Toggle />
-         <Navbar.Collapse className='justify-content-end'>
-           <Navbar.Text>
-             {/* Signed in as: <a href='#login'>Mark Otto</a> */}
-           </Navbar.Text>
-         </Navbar.Collapse>
-       </Navbar>
-       <div className='sidenav'>
-         <div className='login-main-text'>
-           {/* <h2>Application</h2> */}
-           <br></br>
-           <h2>Login Page</h2>
-           <p>Login or register</p>
-         </div>
-      </div>
-      <div className='main'>
-         <div className='col-md-6 col-sm-12'>
-           <div className='login-form'>
-             <form>
-               <div className='form-group'>
-                 <label>User Name</label>
-                 <input
-                   type='text'
-                   onChange={e => setEmail(e.target.value)}
-                   className='form-control'
-                   placeholder='User Name'
-                 ></input>
-               </div>
+        e.preventDefault()
+        fire
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(u => {
+                console.log('usingup', u)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    return (
+        <div>
+            <div className='sidenav'>
+                <div className='login-main-text'>
+                    {/* <h2>Application</h2> */}
+                    <br></br>
+                    <h2>Login Page</h2>
+                    <p>Login or register</p>
+                </div>
+            </div>
+            <div className='main'>
+                <div className='col-md-6 col-sm-12'>
+                    <div className='login-form'>
+                        <form>
+                            <div className='form-group'>
+                                <label>User Name</label>
+                                <input
+                                    type='text'
+                                    onChange={e => setEmail(e.target.value)}
+                                    className='form-control'
+                                    placeholder='User Name'
+                                ></input>
+                            </div>
 
-               <div className='form-group'>
-                 <label>Password</label>
-                 <input
-                   type='password'
-                   onChange={e => setPassword(e.target.value)}
-                   className='form-control'
-                   placeholder='Password'
-                 ></input>
-               </div>
-               <button type='submit' onClick={signin} className='btn btn-black'>
-                 Login
+                            <div className='form-group'>
+                                <label>Password</label>
+                                <input
+                                    type='password'
+                                    onChange={e => setPassword(e.target.value)}
+                                    className='form-control'
+                                    placeholder='Password'
+                                ></input>
+                            </div>
+                            <button type='submit' onClick={signin} className='btn btn-black'>
+                                Login
                </button>
-               <button type='submit' onClick={signup} className='btn btn-secondary'>
-                 Register
+                            <button type='submit' onClick={signup} className='btn btn-secondary'>
+                                Register
                </button>
-             </form>
-           </div>
-         </div>
-       </div>
-       {/* <div className='Login-header'>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            {/* <div className='Login-header'>
          <div>
            <input
              type='text'
@@ -110,8 +101,8 @@ function LoginPage () {
            </button>
          </div>
        </div> */}
-       {/* https://bootsnipp.com/snippets/7nk08 */}
-     </div>
-  )
+            {/* https://bootsnipp.com/snippets/7nk08 */}
+        </div>
+    )
 }
 export default LoginPage;
