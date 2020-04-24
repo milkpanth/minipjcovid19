cae1f59990fd3afc3ae989b93e2d9e67b458b4a0
milkpanth <milkpanth@yahoo.com>
2020-04-24 15:21:02 +0700

    homepage logout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/LoginPage'
 import { Route } from 'react-router-dom';
- import HomePage from './components/HomePage'
+import HomePage from './components/HomePage'
+import fire from './config/fire'
 
-function App() {
-  return (
-    <div >
-       <div>
-         <Route exact path="/" component={HomePage} />
-         <Route path="/login" component={LoginPage} />
-       </div>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {}
+    }
+  }
+  componentDidMount() {
+    this.authListener()
+  }
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user })
+      }
+      else {
+        this.setState({ user: null })
+      }
+    })
+  }
+
+  render() {
+    if (this.state.user == null) {
+      return (
+        <div>
+          <LoginPage />
+        </div>
+
+      );
+    }
+    return (
+      <div>
+        <div>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import fire from '../config/fire';
+
  function HomePage () {
+    const logout = () => {
+        fire.auth().signOut();
+    }
      return(
          <div>
              HomePage
+             <button onClick={logout}>Logout</button>
          </div>
      )
  }
