d7c66b4206df7abfed6d85dc5d28f8d376fac2b1
milkpanth <milkpanth@yahoo.com>
2020-04-24 16:47:45 +0700

    fetch axios

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,26 +8,35 @@ import fetch from 'unfetch'
 import useSWR from 'swr'
 
 const apiUrl = 'https://api.covid19api.com/summary';
-function HomePage() {
-    const fetcher = url => axios.get(url).then(r=>r.json());
-    const { data , error} = useSWR
+// const fetcher = url => fetch(url).then(r => r.json());
+const fetcher = url => axios.get(url)
+const HomePage = () => {
+   
     const logout = () => {
         fire.auth().signOut()
       }
-      return (
-        <div>
-          <Navbar bg='dark' variant='dark'>
-            <Navbar.Brand href='#home'>MiniProjet covid19</Navbar.Brand>
-            <Nav className='mr-auto'>
-              <Nav.Link href='#home'>Home</Nav.Link>
-              <Nav.Link href='#features'>Features</Nav.Link>
-              <Nav.Link href='#pricing'>Pricing</Nav.Link>
-            </Nav>
-            <Form inline>
-              <Button onClick={logout} variant='outline-info'>Logout</Button>
-            </Form>
-            </Navbar>
-            </div>
-   )
+      const { data, error } = useSWR(apiUrl, fetcher);
+
+   if (!data) {
+     return <p>not found</p>
+   } else
+     return (
+       <div>
+         <Navbar bg='dark' variant='dark'>
+           <Navbar.Brand href='#home'>MiniProject covid19</Navbar.Brand>
+           <Nav className='mr-auto'>
+             <Nav.Link href='#home'>Home</Nav.Link>
+             <Nav.Link href='#features'>Features</Nav.Link>
+             <Nav.Link href='#pricing'>Pricing</Nav.Link>
+           </Nav>
+           <Form inline>
+             <Button onClick={logout} variant='outline-info'>
+               Logout
+             </Button>
+           </Form>
+         </Navbar>
+         <p>{JSON.stringify(data)}</p>
+       </div>
+     )
  }
 export default HomePage;
