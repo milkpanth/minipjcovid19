ac2e955bff90975bb1e6b576e6660e5e06b19a3c
milkpanth <milkpanth@yahoo.com>
2020-04-24 14:51:15 +0700

    input username userState

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react'
 function LoginPage () {
-  return(
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  console.log(username)
+  console.log(password)
+  return (
     <div>
-        loginpage
+      LoginPage
+      <div>
+        <input type='text' onChange={e => setUsername(e.target.value)}></input>
+      </div>
+      <div>
+        <input
+          type='password'
+          onChange={e => setPassword(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <button type='submit'>SingIn</button>
+      </div>
     </div>
   )
 }
-export default LoginPage
+export default LoginPage;
