aee9b2c7698ba8fd0b123d9aa09f6cbe900d379d
milkpanth <milkpanth@yahoo.com>
2020-04-26 02:07:43 +0700

    test firebase database

--- src/components/SelfScreening.js
+++ src/components/SelfScreening.js
@@ -3,6 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState, useEffect } from 'react'
 import fire from '../config/fire'
 import { Nav, Navbar, Button, Form } from 'react-bootstrap'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+if (firebase.apps.length === 0)
+    firebase.initializeApp(fire)
+export const firestore = firebase.firestore()
 
 const SelfScreening = () => {
     const [user, setUser] = useState('')
@@ -23,6 +29,38 @@ const SelfScreening = () => {
     const logout = () => {
         fire.auth().signOut()
     }
+    const [tasks, setTasks] = useState([])
+    const [name, setName] = useState('')
+
+    const retriveData = () => {
+        firestore.collection("tasks").orderBy('id', 'asc').onSnapshot(snapshot => {
+            let myTask = snapshot.docs.map(d => {
+                const { id, name } = d.data()
+                return { id, name }
+            })
+            setTasks(myTask)
+        })
+    }
+
+    useEffect(() => {
+        retriveData()
+    }, [])
+
+    const renderTask = () => {
+        if (tasks && tasks.length) {
+            return tasks.map((task, index) => (
+                <li key={index}>{task.id}:{task.name} </li>
+            ))
+        }
+        else {
+            return <div>No Task</div>
+        }
+    }
+    const addTask = () => {
+        let id = (tasks.length === 0) ? 1 : tasks[tasks.length - 1].id + 1
+        firestore.collection("tasks").doc(id + '').set({ id, name })
+    }
+
     return (
         <div>
             <Navbar bg='dark' variant='dark'>
@@ -40,6 +78,10 @@ const SelfScreening = () => {
              </Button>
                 </Form>
             </Navbar>
+            <h1>Todo</h1>
+            <input type="text" name="name" onChange={(e) => setName(e.target.value)}></input>
+            <button onClick={addTask}>Submit</button>
+            <ul > {renderTask()}</ul>
             <div className='container'>
                 <style jsx>
                     {`
