83cf98828b52f00905bc62780d512e9808f85a76
milkpanth <milkpanth@yahoo.com>
2020-04-26 01:14:44 +0700

    SelfScreening css

--- src/components/SelfScreening.js
+++ src/components/SelfScreening.js
@@ -25,7 +25,7 @@ const SelfScreening = () => {
                 {`
               .container {
                 @import url('https://fonts.googleapis.com/css2?family=Baloo+Bhaina+2:wght@500&display=swap');
-                font-family: 'Baloo Bhaina 2', cursive;
+                font-family: 'Arial', cursive;
                 width: 820px;
                 margin: 0 auto;
               }
@@ -39,7 +39,7 @@ const SelfScreening = () => {
             <h1>SelfScreening</h1>
             <from>
                 <div class='form-group row'>
-                    <label for='staticEmail' class='col-sm-2 col-form-label'>
+                    <label for='staticEmail' class='col-sm-10 col-form-label'>
                         Email
               </label>
                     <div class='col-sm-10'>
@@ -52,9 +52,63 @@ const SelfScreening = () => {
                         ></input>
                     </div>
                 </div>
+
                 <div class='form-group row'>
-                    <label for='staticEmail' class='col-sm-2 col-form-label'>
-                        Email
+                    <label for='staticEmail' class='col-sm-10 col-form-label'>
+                        เพศ (Sex)
+           </label>
+                    <div class='col-sm-10'>
+                        <select class='custom-select'>
+                            <option selected>Open this select menu</option>
+                            <option value='1'>One</option>
+                            <option value='2'>Two</option>
+                            <option value='3'>Three</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class='form-group row'>
+                    <label for='staticEmail' class='col-sm-10 col-form-label'>
+                        1. ท่านมีไข้หรือไม่ Do you have Fever?
+           </label>
+                    <div class='col-sm-10'>
+                        <select class='custom-select'>
+                            <option selected>Open this select menu</option>
+                            <option value='1'>One</option>
+                            <option value='2'>Two</option>
+                            <option value='3'>Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div class='form-group row'>
+                    <label for='staticEmail' class='col-sm-10 col-form-label'>
+                        2. ท่านมีอาการดังต่อไปนี้ หรือไม่ Do you have any of these Symptoms?
+           </label>
+                    <div class='col-sm-10'>
+                        <select class='custom-select'>
+                            <option selected>Open this select menu</option>
+                            <option value='1'>One</option>
+                            <option value='2'>Two</option>
+                            <option value='3'>Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div class='form-group row'>
+                    <label for='staticEmail' class='col-sm-10 col-form-label'>
+                        3. ท่านมีประวัติเดินทางมาจากประเทศจีน, ญี่ปุ่น, สิงคโปร์, เกาหลีใต้, ฮ่องกง, ไต้หวัน, มาเก๊า ,เยอรมัน, ฝรั่งเศส หรือในพื้นที่ที่มีการระบาดของโรคไวรัสโคโรนา สายพันธุ์ใหม่ 2019 ในช่วงเวลา 14 วัน ก่อนเริ่มป่วย ใช่หรือไม่ ? Do you have traveled from China, Japan, Singapore, Republic of Korea, HongKong, Taiwan, Macao, Germany, France or the Novel Coronavirus 2019 outbreak areas within the past 14 days before get sick ?
+           </label>
+                    <div class='col-sm-10'>
+                        <select class='custom-select'>
+                            <option selected>Open this select menu</option>
+                            <option value='1'>One</option>
+                            <option value='2'>Two</option>
+                            <option value='3'>Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div class='form-group row'>
+                    <label for='staticEmail' class='col-sm-10 col-form-label'>
+                        4. ท่านมีประวัติสัมผัสใกล้ชิดกับผู้ป่วยที่ต้องสงสัยการติดเชื้อโรคไวรัสโคโรนาสายพันธุ์ใหม่ 2019 หรือมีอาชีพที่มีโอกาสสัมผัสนักท่องเที่ยวต่างชาติ หรือไม่? Do you have contacted with suspected or have career opportunities with foreign tourists?
            </label>
                     <div class='col-sm-10'>
                         <select class='custom-select'>
